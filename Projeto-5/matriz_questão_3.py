6b477199e1375cae6f3e44dca9423318052e2d6c
Amanda Costa Silva <79948004+amanda-cs@users.noreply.github.com>
2021-07-01 23:20:30 -0300

    Add files via upload
    ESSE É O CERTO

--- Projeto-5/matriz_questão_3.py
+++ Projeto-5/matriz_questão_3.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/1z9L_pPRyaJ2Jq3R4oB3LToffXcItkWp6
 """
 
+import numpy as np
+
 A = [
     [0, .5, .5, 0, 0, 0, 0, 0],
     [0, 0, .5, .5, 0, 0, 0, 0],
@@ -17,12 +19,6 @@ A = [
     [0, 0, 0, 0, 0, 0, 0, .5],
     [.5, 0, .5, 0, 0, 0, 0, 0]
 ]
-
-A_t = np.transpose(A)
-
-print(A_t)
-
-import numpy as np
 
 X = [
     [[x1], [x2], [x3], [x4], [x5], [x6], [x7], [x8]]
